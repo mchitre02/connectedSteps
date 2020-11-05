5e083e0596565ad05f5618771f24404232e1a93e
mchitre02 <markchitre@gmail.com>
2020-11-05 01:31:20 -0500

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,30 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type TabThreeParamList = {
+  TabThreeScreen: undefined;
+};
+
+export type TabFourParamList = {
+  TabThreeScreen: undefined;
+};
+
+export type TabFiveParamList = {
+  TabThreeScreen: undefined;
+};
+
